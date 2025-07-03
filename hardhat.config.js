172ec7f333835f593552ce327dc6a6e9012c8012
Z <50341426+ZNav@users.noreply.github.com>
2025-07-03 04:09:05 +0000

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY], // set your private key in env
+    },
+  },
+};
